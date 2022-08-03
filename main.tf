fbdb1d69f88b42bfa4aebf9432addf06ffae00a6
Sandro Flausino <sjflausino@gmail.com>
2022-08-03 16:42:16 -0300

    teste branch2

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region        = "us-east-1"
   access_key    = var.aws_access_key_id
