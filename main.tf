31ab58ea8aeefb41d7f2977cd84eeeecc32363af
Sandro Flausino <sjflausino@gmail.com>
2022-08-03 16:41:27 -0300

    teste branch2

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  cloud {
-    organization = "sjflausino"
-    workspaces {
-      name = "VKPR-759"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
   region        = "us-east-1"
   access_key    = var.aws_access_key_id
