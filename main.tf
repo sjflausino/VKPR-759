7db3a6860467d2e2fb8290bd1b6b06a0dfed2696
Sandro Flausino <sjflausino@gmail.com>
2022-08-04 14:20:44 -0300

    acho que agora vai

--- main.tf
+++ main.tf
@@ -1,29 +1,31 @@
 terraform {
-  required_version = ">= 0.14.9"
+  required_version   = ">= 0.14.9"
   cloud {
-    organization = "sjflausino"
+    organization     = "sjflausino"
     workspaces {
-      name = "VKPR-759"
+      name           = "VKPR-759"
     }
   }
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+    aws              = {
+      source         = "hashicorp/aws"
+      version        = "~> 4.0"
     }
   }
 }
-
 provider "aws" {
-  region        = "us-east-1"
-  
+  region     = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
-
-resource "aws_instance" "web" {
-  ami           = "ami-090fa75af13c156b4"
+resource "aws_vpc" "VKPR-759" {
+  cidr_block = "0.0.0.0"
+}
+resource "aws_instance" "teste" {
+  ami                = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
     tags = {
-    Name = "HelloWorld"
+    Name             = "HelloWorld"
   }
 }
 
