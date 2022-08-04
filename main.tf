6d00674e887cd6d73e74d55e0c9be24405128cae
Sandro Flausino <sjflausino@gmail.com>
2022-08-04 15:50:21 -0300

    AGORA VAI MESMO

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 terraform {
-  required_version   = ">= 0.14.9"
-  cloud {
-    organization     = "sjflausino"
-    workspaces {
-      name           = "VKPR-759"
-    }
-  }
+  required_version   = ">= 1.0.0"
   required_providers {
     aws              = {
       source         = "hashicorp/aws"
