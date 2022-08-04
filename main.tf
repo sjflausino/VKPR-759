ddd9b1e6af88a99bfca9d534022e984182259303
Sandro Flausino <sjflausino@gmail.com>
2022-08-04 13:41:07 -0300

    ERA A MERDA DA AMI??????

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-020ef1e2f6c2cc6d6"
+  ami           = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
     tags = {
     Name = "HelloWorld"
