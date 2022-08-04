f44e4e546045dc80157a19c9977e464fed04e582
Sandro Flausino <sjflausino@gmail.com>
2022-08-04 14:02:26 -0300

    não era t.t

--- main.tf
+++ main.tf
@@ -10,11 +10,13 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
-      region        = "us-east-1"
-      access_key    = var.aws_access_key_id
-      secret_key    = var.aws_secret_access_key
     }
   }
+}
+
+provider "aws" {
+  region        = "us-east-1"
+  
 }
 
 resource "aws_instance" "web" {
