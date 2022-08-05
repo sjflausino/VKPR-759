682a73497570dd2dbc3bcbf13f35d2b6108f4ebf
Sandro Flausino <sjflausino@gmail.com>
2022-08-05 17:35:55 -0300

    adicionando bucket

--- main.tf
+++ main.tf
@@ -19,10 +19,16 @@ resource "aws_instance" "teste" {
     Name             = "VKPR-759"
   }
 }
-
-resource "aws_s3_bucket" "VKPR-759" {
+resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
+
   tags = {
     Name        = "My bucket"
+    Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
 }
